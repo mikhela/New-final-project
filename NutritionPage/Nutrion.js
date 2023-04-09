6620ef94774dcd14a9c568654a0fcb501eef10d4
mikhela <giksona151@gmail.com>
2023-04-09 14:47:29 +0400

    Giorgi

---
+++ NutritionPage/Nutrion.js
@@ -0,0 +1,23 @@
+$(".FirstB").click(function (e) { 
+    e.preventDefault();
+    $(".shemcvale").hide()
+    $(".shecvlilze").show()
+    $(".shecvlilze2").hide()
+    $(".unforhundred").show()
+    $(".unfivehundred").hide()
+    $(".first__row__290cal").show()
+    $(".second__row__500cal").hide()
+});
+
+$(".SecB").click(function (e) { 
+    e.preventDefault();
+    $(".shemcvale").hide()
+    $(".shecvlilze").hide()
+    $(".shecvlilze2").show()
+    $(".unforhundred").hide()
+    $(".unfivehundred").show()
+    $(".first__row__290cal").hide()
+    $(".second__row__500cal").show()
+
+});
+
