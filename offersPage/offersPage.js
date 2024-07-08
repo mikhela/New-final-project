c15b646d800821550f42218d77f024fd7e0c5cdd
unknown <giksona151@gmail.com>
2024-07-09 02:10:41 +0400

    newcom

--- offersPage/offersPage.js
+++ offersPage/offersPage.js
@@ -1,23 +1,21 @@
-$(".content").click(function (e) { 
-    e.preventDefault();
-    $(".Promocode__modal").show()
-    $("footer").hide()
+$(".content").click(function (e) {
+  e.preventDefault();
+  $(".Promocode__modal").show();
+  $("footer").hide();
 });
-$("#close").click(function (e) { 
-    e.preventDefault();
-    $(".Promocode__modal").hide()
+$("#close").click(function (e) {
+  e.preventDefault();
+  $(".Promocode__modal").hide();
 });
 
-
-$("#buttonClick").click(function (e) { 
-    e.preventDefault();
-    $(".myInPut").val("")
-    $("#gamochndi").show()
+$("#buttonClick").click(function (e) {
+  e.preventDefault();
+  $(".myInPut").val("");
+  $("#gamochndi").show();
 });
 
 var navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", function () {
-    navbar.classList.toggle("sticky", window.scrollY > 0)
-})
-
+  navbar.classList.toggle("fixed", window.scrollY > 0);
+});
